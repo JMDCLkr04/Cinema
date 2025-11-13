e6ced638c1dd0a33320427339f258e61592317a7
marco <marcoadrianzambra@gmail.com>
2025-11-13 01:04:20 -0500

    fix: login and register small style changes

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -2,12 +2,14 @@ import { LoginForm } from "@/components/auth/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-background to-muted/20 lg:p-8 flex w-full flex-col justify-center align-center mx-auto space-y-6 sm:w-[400px] text-center">
+    <div className="min-h-screen bg-gradient-to-b from-background to-muted/20 lg:p-8 flex w-full flex-col justify-center align-center mx-auto space-y-6 sm:w-[500px] text-center">
       <h1 className="text-3xl font-bold tracking-tight">Bienvenido de nuevo</h1>
       <p className="text-sm text-muted-foreground">
         Ingresa tus credenciales para acceder a tu cuenta
       </p>
-      <LoginForm />
+      <div className="flex justify-center space-y-2 text-center w-full">
+        <LoginForm />
+      </div> 
     </div>
   )
 }
--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -3,13 +3,13 @@ import { RegisterForm } from "@/components/auth/register-form"
 export default function RegisterPage() {
   return (
     <div className="bg-gradient-to-b from-background to-muted/20 mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px] container relative min-h-screen items-center lg:max-w-none lg:px-0 lg:p-8">
-        <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-3xl font-bold tracking-tight">Crea tu cuenta</h1>
-          <p className="text-sm text-muted-foreground">
-            Ingresa tus datos para crear una cuenta
-          </p>
-        </div>
-        <RegisterForm />
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="text-3xl font-bold tracking-tight">Crea tu cuenta</h1>
+        <p className="text-sm text-muted-foreground">
+          Ingresa tus datos para crear una cuenta
+        </p>
+      </div>
+      <RegisterForm />
     </div>
   )
 }
